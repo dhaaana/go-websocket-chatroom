a7a068ea5c3d73757508e1d71f46437f5a1f0fa3
Ramadhana R <ddhanarr@gmail.com>
2023-02-02 01:16:04 +0700

    feat: landing page and chat room frontend

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/dhaaana/go-websocket-chatroom/config"
+)
 
 func main() {
-	fmt.Printf("Hello, world.")
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	http.HandleFunc("/", serveLanding)
+	http.HandleFunc("/chat", serveChatRoom)
+
+	fmt.Println("Server running at port " + config.Port)
+	http.ListenAndServe(":"+config.Port, nil)
+}
+
+func serveLanding(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("public/landing.html")
+	if err != nil {
+		http.Error(w, "Could not open requested file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", content)
+}
+
+func serveChatRoom(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("public/chat.html")
+	if err != nil {
+		http.Error(w, "Could not open requested file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", content)
 }
